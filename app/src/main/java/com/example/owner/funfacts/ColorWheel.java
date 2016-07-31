835270dba46f9f78005b7b4e24bb19fa788e4702
helplessStruggle <kresco96@gmail.com>
2016-07-31 20:21:28 +0200

    FactsApp

---
+++ app/src/main/java/com/example/owner/funfacts/ColorWheel.java
@@ -0,0 +1,35 @@
+package com.example.owner.funfacts;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+public class ColorWheel {
+    // Fields (Member Variables) - Properties about the object
+    private String[] mColors = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+    public int getColor() {
+        // Randomly select a fact
+        String color;
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(mColors.length);
+        color = mColors[randomNumber];
+        int colorAsInt = Color.parseColor(color);  // We need to convert the string to int, since SetBackgroundColor expects int
+
+        return colorAsInt;
+    }
+}
---
+++ app/src/main/java/com/example/owner/funfacts/FunFactsActivity.java
@@ -0,0 +1,52 @@
+package com.example.owner.funfacts;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
+    // Declare our View variables
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Assign the Views from the layout file to the corresponding variables
+        mFactTextView = (TextView) findViewById(R.id.fact);
+        mShowFactButton = (Button) findViewById(R.id.ShowFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                int color = mColorWheel.getColor();
+                // Update the screen with out dynamic fact + background color
+                mFactTextView.setText(fact);
+                mRelativeLayout.setBackgroundColor(color);
+                mShowFactButton.setTextColor(color);
+            }
+        } ;
+
+        mShowFactButton.setOnClickListener(listener);
+
+        Log.d(TAG, "We're logging from the onCreate() method");
+    }
+}
